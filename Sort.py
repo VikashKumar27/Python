bf8e5a873c3293f430014978eb6ea02635d27394
Vikash <vikash00mot@gmail.com>
2023-05-27 22:56:07 +0530

    initial commit

--- Sort.py
+++ Sort.py
@@ -1,3 +1,20 @@
 list1 = [100,1,2,3,4,5]
 list1.sort()
 print(list1)
+
+list1 = [100,1,2,3,4,5]
+list1.sort(reverse=True)
+print(list1)
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana","Apple" ,"Cat"]
+thislist.sort()
+print(thislist)
+
+# string is a sequence of characters
+#strings comes unders doubles quotes
+# some functions of strings like - lstrip() , rstrip() , len , sort
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana","Apple" ,"Cat","z-index","xyz","yx"]
+thislist.sort(reverse= True)
+print(thislist)
+
