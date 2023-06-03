75260a3ba49c979e9b98690d7ccc50d3c7f45922
Vikash <vikash00mot@gmail.com>
2023-06-03 10:32:10 +0530

    initial commit

---
+++ Function1.py
@@ -0,0 +1,13 @@
+# def add(a,b):
+#     print(a+b)
+
+# add(10,20) 
+
+def add():
+    a = int(input("Enter the first no : "))
+    b = int(input("Enter the secod no : "))
+    c = a+b
+    print(c)
+add()
+
+
