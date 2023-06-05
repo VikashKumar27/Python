612e5edd5e4fc8c892c688b0b23b27a5e21a2c8b
Vikash <vikash00mot@gmail.com>
2023-06-05 16:50:20 +0530

    initial commit

--- Database1.py
+++ Database1.py
@@ -13,14 +13,18 @@ myconn = mysql.connector.connect(host = "localhost", user = "root",passwd="1234"
   
 mycursor = myconn.cursor()
 
-# mycursor.execute("CREATE TABLE Employee_Details (name VARCHAR(255), address VARCHAR(255),country varchar(255))")
-sql = "INSERT INTO employee_details (name, address, country) VALUES (%s, %s, %s)"
-# val = ("John", "Highway 21","India")
+# mycursor.execute("CREATE TABLE Employee_Details (employee_id integer , name VARCHAR(255), address VARCHAR(255),country VARCHAR(255),company VARCHAR(255),Department varchar(255),salary VARCHAR(255))")
+sql = "INSERT INTO employee_details (employee_id,name, address, country,company,Department, Salary) VALUES (%s, %s, %s,%s,%s,%s,%s)"
+val = ("John", "Highway 21","India")
 for i in range(2,rows+1):
-    name = worksheet1.cell(row=i,column=1).value.strip()
-    address = worksheet1.cell(row=i,column=2).value.strip()
-    country = worksheet1.cell(row=i,column=3).value.strip()
-    val =(name,address,country)
+    employee_id = worksheet1.cell(row=i,column=1).value
+    name = worksheet1.cell(row=i,column=2).value.strip()
+    address = worksheet1.cell(row=i,column=3).value.strip()
+    country = worksheet1.cell(row=i,column=4).value.strip()
+    company = worksheet1.cell(row=i,column=5).value.strip()
+    Department = worksheet1.cell(row=i,column=6).value.strip()
+    Salary = worksheet1.cell(row=i,column=7).value.strip()
+    val =(employee_id,name,address,country,company,Department,Salary)
     mycursor.execute(sql, val)
     myconn.commit()
     # print(mycursor.rowcount, "record inserted.")
